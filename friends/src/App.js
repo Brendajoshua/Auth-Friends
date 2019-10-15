82f768ee03dc08a20aa84a9cf6c21dbe900a419a
Brenda Joshua <brendajosh.bj@gmail.com>
2019-10-15 16:52:53 +0300

    built friendslist component and setup links in app

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Route } from "react-router-dom";
-
+import { Route, Link } from "react-router-dom";
 import './App.css';
 import Login from './components/Login';
+import FriendsList from './components/FriendsList';
 
 function App() {
   return (
     <div className="App">
+      <Link to="/login">Login</Link>
+      <Link to="/friends">Friends</Link>
       <Route path="/login" component={Login} />
+      <Route path="/friends" component={FriendsList} />
     </div>
   );
 }

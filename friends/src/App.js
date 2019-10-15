062f61286ffbf87aaaa98ae1b31f339d3ed6cb44
Brenda Joshua <brendajosh.bj@gmail.com>
2019-10-15 18:08:26 +0300

    created logout functionality

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState} from "react";
 import { Route, Link } from "react-router-dom";
 import './App.css';
 import Login from './components/Login';
@@ -6,15 +6,36 @@ import FriendsList from './components/FriendsList';
 import PrivateRoute from './components/PrivateRoute';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(localStorage.getItem('token'));
+
+  const logout = () => {
+    localStorage.clear();
+    setLoggedIn(false)
+  }
+
+  const login = () => {
+    setLoggedIn(true);
+  }
   return (
     <div className="App">
       <div class="links-container">
         <div className="links">
-          <Link to="/login"><button>Login</button></Link>
-          <Link to="/friends"><button>Friends</button></Link>
+        {!loggedIn && (
+            <Link to="/login">
+              <button>Login</button>
+            </Link>
+          )}
+          {loggedIn && (
+            <Link to="/login" onClick={() => logout()}>
+              <button>Logout</button>
+            </Link>
+          )}
+          <Link to="/friends">
+            <button>Friends</button>
+          </Link>
         </div>
       </div>
-      <Route path="/login" component={Login} />
+      <Route path="/login" render={(props) => <Login {...props} login={Login} />} />
       <PrivateRoute path="/friends" component={FriendsList} />
     </div>
   );

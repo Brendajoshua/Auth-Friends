929565a803220c1e5569e2da43ba09ca3be490a0
Brenda Joshua <brendajosh.bj@gmail.com>
2019-10-15 16:59:12 +0300

    changed friends route to privateroute

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from "react-router-dom";
 import './App.css';
 import Login from './components/Login';
 import FriendsList from './components/FriendsList';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Link to="/login">Login</Link>
       <Link to="/friends">Friends</Link>
       <Route path="/login" component={Login} />
-      <Route path="/friends" component={FriendsList} />
+      <PrivateRoute path="/friends" component={FriendsList} />
     </div>
   );
 }
